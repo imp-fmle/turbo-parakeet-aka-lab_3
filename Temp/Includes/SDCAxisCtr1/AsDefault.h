7e33e1bfd5ff368f2210224137a36e6c145c91dd
i.pavlov <i.pavlov@inspector-cloud.com>
2023-12-15 15:28:19 +0300

    add file

---
+++ Temp/Includes/SDCAxisCtr1/AsDefault.h
@@ -0,0 +1,7 @@
+#ifndef _SDCAXISCTR1_DEFAULT_607957842
+#define _SDCAXISCTR1_DEFAULT_607957842
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <variablesVAR.h>
+#endif
---
+++ Temp/Includes/SDCAxisCtr2/AsDefault.h
@@ -0,0 +1,7 @@
+#ifndef _SDCAXISCTR2_DEFAULT_958126099
+#define _SDCAXISCTR2_DEFAULT_958126099
+#include "../AsDefault.h"
+#include <bur/plctypes.h>
+#include <bur/plc.h>
+#include <variablesVAR.h>
+#endif
